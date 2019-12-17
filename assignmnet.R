5d80780bf95e1e3e04aa6b29f7c5af06b133446f
Rashbir Singh <rashbits@gmail.com>
2019-12-17 11:11:03 +0530

    Change in code and added PNG
    Added code for plotting data and added output PNGs for each plot

--- assignmnet.R
+++ assignmnet.R
@@ -15,14 +15,64 @@ rownames(df_clean) = c(1:nrow(df_clean))
 
 write.csv(df_clean, 'output.csv')
 
+###################################################
+# EXPLORING THE DATA                              #
+###################################################
+
 print(paste0("Mean = ",mean(df_clean$Value)))
 print(paste0("Variance = ", var(df_clean$Value)))
+print(paste0(c('Minimum Temp = ', 'Average = ', 'Year of Minimum Temp = '), 
+             df_clean[c(df_clean$Value == min(df_clean$Value)),]))
+print(paste0(c('Maximum Temp = ', 'Average = ', 'Year of Maximum Temp = '),
+             df_clean[c(df_clean$Value == max(df_clean$Value)),]))
 
 ###################################################
 # PLOTTING                                        #
 ###################################################
 
-par(mfrow = c(1, 2))
+### HISTOGRAM ###
 hist(df_clean$Value, 
-     xlab = 'Time')
+     xlab = 'Time',
+     xaxt='n',
+     yaxt='n',
+     main = 'Temperature from year 1999-2019 in Fahrenheit',
+     cex.main=0.8)
+axis(side=1, at=c(1:nrow(df_clean)), labels=c(df_clean$Timeseries),
+     lwd=0.1,
+     line = NA,
+     cex.axis=0.4)
+axis(side=2, lwd=0.1,
+     cex.axis=0.4)
+
+### Desnity Plot ###
 plot(density(df_clean$Value))
+
+### Weather Plot ###
+plot(c(1:nrow(df_clean)), 
+     df_clean$Value,
+     xlab = 'Time',
+     ylab = 'Value',
+     type = 'S',
+     xaxt = 'n',
+     yaxt = 'n')
+axis(side=1, at=c(1:nrow(df_clean)), labels=c(df_clean$Timeseries),
+     lwd=0.1,
+     cex.axis=0.4)
+axis(side=2, at=c(1:228), labels=seq(min(df_clean$Value), max(df_clean$Value), 1),
+     lwd=0.1,
+     cex.axis=0.4)
+abline(h=mean(df_clean$Value), col='blue')
+abline(h=min(df_clean$Value), col='red')
+abline(h=max(df_clean$Value), col='green')
+
+add_legend <- function(...) {
+        opar <- par(fig=c(0, 1, 0, 1), oma=c(0, 0, 0, 0), 
+                    mar=c(0, 0, 0, 0), new=TRUE)
+        on.exit(par(opar))
+        plot(0, 0, type='n', bty='n', xaxt='n', yaxt='n')
+        legend(...)
+} ## Reference - https://stackoverflow.com/questions/3932038/plot-a-legend-outside-of-the-plotting-area-in-base-graphics
+
+add_legend("topright", legend=c("Min", "Max", "Mean"), pch=20, 
+           col=c("red", "green", "blue"),
+           horiz=TRUE, bty='n', cex=0.8)
